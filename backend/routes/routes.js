cb3c7ed62aef66c6eb99099d79988753fc909087
Rokol <biel_s.carvalho2@hotmail.com>
2022-04-22 15:52:59 -0300

    SignUp Post

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels')
 
-router.post('/singup', (request, response) => {
+router.post('/signup', (request, response) => {
     const signedUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose =require('mongoose')
 const dotenv = require('dotenv')
-const routesUrls = require('./routes/routes')
+const routesUrls = require('./routes/routes.js')
+const cors = require('cors')
 
 
 dotenv.config()
@@ -10,5 +11,8 @@ dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
 
+app.use(express.json())
+app.use(cors())
 app.use('/api', routesUrls)
 app.listen(4000, () => console.log("Server is up and running"))
+ 
